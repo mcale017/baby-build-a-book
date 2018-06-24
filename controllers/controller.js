4ce2497bad2e57d4e8141ad20f6b3703b80130dd
Ryan <mcale017@umn.edu>
2018-06-24 10:23:46 -0500

    fixed controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -10,48 +10,29 @@ var book = require("../models/book.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
     book.selectAll(function (data) {
-        var bookObject = {
+        var booksObject = {
             books: data
         };
-        console.log(bookObject);
+        console.log(booksObject);
         
-        res.json(bookObject);
+        res.render("index", booksObject)
     });
 });
 
 router.get("/api/books", function (req, res) {
-    burger.selectAll(function (data) {
-        var hbsObject = {
-            burgers: data
+    book.selectAll(function (data) {
+        var booksObject = {
+            books: data
         };
-        res.json(hbsObject);
+        res.json(booksObject);
     })
 })
 
-router.post("/api/burgers", function (req, res) {
-    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
-        // Send back the ID of the new burger
+router.post("/api/books", function (req, res) {
+    burger.insertOne(["book_name", "category"], [req.body.book_name, req.body.category], function (result) {
+        // Send back the ID of the new book
         res.json({ id: result.insertId });
     });
-});
-
-router.put("/api/burgers/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
-
-    burger.updateOne(
-        {
-            devoured: req.body.devoured
-        },
-        condition,
-        function (result) {
-            if (result.changedRows === 0) {
-                // If no rows were changed, then the ID must not exist, so 404
-                return res.status(404).end();
-            }
-            res.status(200).end();
-
-        }
-    );
 });
 
 // Export routes for server.js to use.
