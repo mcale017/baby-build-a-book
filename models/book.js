31bcbd2d65f7aa5df5175c2e8ee006ceff291405
Ryan <mcale017@umn.edu>
2018-06-24 21:45:01 -0500

    handlebar in server.js

--- models/book.js
+++ models/book.js
@@ -2,21 +2,21 @@
 var orm = require("../config/orm.js");
 
 var book = {
-  selectAll: function(cb) {
-    orm.selectAll("books", function(res) {
-      cb(res);
-    });
-  },
-  selectOne: function(condition, cb) {
-    orm.selectOne("books", condition, function(res) {
-        cb(res);
-    });
-  },
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne("books", cols, vals, function(res) {
-      cb(res);
-    });
-  }
+    selectAll: function (cb) {
+        orm.selectAll("books", function (res) {
+            cb(res);
+        });
+    },
+    selectOne: function (condition, cb) {
+        orm.selectOne("books", condition, function (res) {
+            cb(res);
+        });
+    },
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("books", cols, vals, function (res) {
+            cb(res);
+        });
+    }
 };
 
 // Export the database functions for the controller
