a530185f5540880a3b2399cc089697cdc33f3612
Ryan <mcale017@umn.edu>
2018-06-26 00:20:16 -0500

    .env added

--- server.js
+++ server.js
@@ -29,6 +29,9 @@ app.set("view engine", "handlebars");
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
+// Require .env to allow unique password
+require('dotenv').config();
+
 // Syncing our sequelize models and then starting our Express app
 db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
